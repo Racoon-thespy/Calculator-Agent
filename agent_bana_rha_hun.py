f866d827c4b6d9de0b6b75a1740f6581f33e1805
Racoon-thespy <pratapsinghaditya82@gmail.com>
2025-06-12 21:29:50 +0530

    initial commit

---
+++ agent_bana_rha_hun.py
@@ -0,0 +1,40 @@
+import os
+import google.generativeai as genai
+#gemini api = AIzaSyCld-u0s85S4gKYrHYQBP7aNsZOjhxA3OE
+
+genai.configure(api_key = "AIzaSyCld-u0s85S4gKYrHYQBP7aNsZOjhxA3OE")
+model = genai.GenerativeModel("gemini-1.5-flash")
+def calculator_baba(expression:str)->str:
+    try:
+        result = eval(expression)
+        return str(result)
+    except Exception as e:
+        return f"kuchh bakchodi ho gyi : {e}"
+def extract_expression(query:str)->str:
+    prompt = f"""You are a math assistant and your job is to extract mathematical expressions
+    that can be solved by python's eval() function out of text queries or world problems.
+    For example, if the user asks - 'What is the sum of all one digit natural numbers',
+    then the expression would be '1+2+3+4+5+6+7+8+9'. 
+    Another example, if the user asks - 'What is 10 percent of the surface of sun's temperature?'
+    then, the expression would be '10/100*5500'.
+    ***You are strictly urged to only return the python compatible eval() function expression!***
+    
+    Input: {query}
+    Output:"""
+    response = model.generate_content(prompt)
+    return response.text.strip("`\n")
+def get_final_response(query:str):
+    expr = extract_expression(query)
+    print(f"The extracted expresion is - {expr}")
+    result = calculator_baba(expr)
+    prompt = f"""User asked for this : {query}
+                Math expression is : {expr}
+                Result is : {result}
+
+                Now, please give a user friendly and readable final output to display to the user."""
+    response=  model.generate_content(prompt)
+    return response.text.strip()
+query = "What is the 1 percent of the population of the world"
+final_answer = get_final_response(query)
+print("Le re lund ke:", final_answer)
+ 
