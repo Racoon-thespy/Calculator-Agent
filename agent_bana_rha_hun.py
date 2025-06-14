a6c725d6d12d2e2d5f23abdd493caa268293f898
Racoon-thespy <pratapsinghaditya82@gmail.com>
2025-06-14 23:04:57 +0530

    Agent now asks for prompts and questions  with added error handling architecture for more user friendly utility!!

--- agent_bana_rha_hun.py
+++ agent_bana_rha_hun.py
@@ -1,20 +1,24 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from google.api_core.exceptions import ServiceUnavailable
 
 load_dotenv()
-
 
 API_KEY = os.getenv("GOOGLE_API_KEY")
 print("API_KEY LOADED IS:", API_KEY)
 genai.configure(api_key = API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
+
+
 def calculator_baba(expression:str)->str:
     try:
         result = eval(expression)
         return str(result)
     except Exception as e:
         return f"kuchh bakchodi ho gyi : {e}"
+    
+
 def extract_expression(query:str)->str:
     prompt = f"""You are a math assistant and your job is to extract mathematical expressions
     that can be solved by python's eval() function out of text queries or world problems.
@@ -26,8 +30,14 @@ def extract_expression(query:str)->str:
     
     Input: {query}
     Output:"""
-    response = model.generate_content(prompt)
-    return response.text.strip("`\n")
+    try:
+        response = model.generate_content(prompt)
+        return response.text.strip("`\n")
+    except ServiceUnavailable:
+        print(" Gemini is overloaded. Chill for a bit and try again later.")
+        return "MODEL_OVERLOADED"
+    
+    
 def get_final_response(query:str):
     expr = extract_expression(query)
     print(f"The extracted expresion is - {expr}")
@@ -39,7 +49,26 @@ def get_final_response(query:str):
                 Now, please give a user friendly and readable final output to display to the user."""
     response=  model.generate_content(prompt)
     return response.text.strip()
-query = "What is the 1 percent of the population of the world"
-final_answer = get_final_response(query)
-print("Le re lund ke:", final_answer)
+
+
+while(True):
+    try:
+        query = input("Hello there, what doubts can I help with? Just lay it on me :)").strip()
+        if query.lower() in ['exit', 'quit', 'bye']:
+            print("Alright, Im done here, hope I helped!!")
+            break
+        final_answer = get_final_response(query)
+
+        if final_answer == "MODEL_OVERLOADED":
+            print("System is overloaded!! Please chill out, smoke a malboro and try again later.")
+            continue
+        print("Here is youur answer : ", final_answer)
+
+    except KeyboardInterrupt:
+        print("Alright, lets terminate this sesh!")
+        break
+
+    except Exception as e:
+        print("There seems to be a problem : {e}")
+
  
